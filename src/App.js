f9325e0e29b98c943fd64be963b629769cba2608
Felix-Medrano <sckrool@gmail.com>
2021-03-26 18:39:39 -0600

    agregado de fondo y Fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="azul"></div>
+      <div className="rojo"></div>
     </div>
   );
 }
